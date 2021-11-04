feb98cec1ec1dc498a958074d0750bb7a5cbf457
Christopher Boyle <chrismjboyle@gmail.com>
2021-11-04 12:30:56 +0000

    Add mimo rendering - but only when there is a single common critical path
    This critical path is used to position nodes up- and down-stream from here

--- mvp/visualiser.py
+++ mvp/visualiser.py
@@ -6,7 +6,6 @@ from .data_node import DataNode, DataNode1D
 from .edge import Edge
 from .util import compile_tex
 from .group import Group
-from .layer import Layer
 
 
 class Visualiser:
@@ -82,57 +81,97 @@ class Visualiser:
         self.groups.append(Group(name, nodes, **kwargs))
 
     def position_nodes(self):
-        root = [n for n, deg in self._graph.out_degree() if not deg][0]
-        self.graph2tree(root, 'down')
-        spacing_x, spacing_y = self.spacing
-        for node in root:
-            y = 0
-            if node.parent:
-                siblings_and_cousins = node.get_siblings_and_cousins()
-                if len(siblings_and_cousins) > 1:
-                    idx = siblings_and_cousins.index(node)
-                    dys = [n.size[1] + spacing_y for n in siblings_and_cousins]
-                    span = sum(dys)
-                    cumu_dys = [dys[0]]
-                    for dy in dys[1:]:
-                        cumu_dys.append(cumu_dys[-1] + dy)
-                    for i, dy in enumerate(dys):
-                        cumu_dys[i] -= dy/2
-                    y = cumu_dys[idx] - span/2
-                else:
-                    y = node.parent.pos[1]
-            node.pos = [-node.get_level()*spacing_x, y]
+        inputs = [n for n, deg in self._graph.in_degree() if not deg]
+        outputs = [n for n, deg in self._graph.out_degree() if not deg]
+
+        top_node = bottom_node = None
+        if len(inputs) > 1 and len(outputs) > 1:
+            common_nodes = None
+            for i in inputs:
+                for o in outputs:
+                    path = nx.shortest_path(self._graph, i, o)
+                    if common_nodes is None:
+                        common_nodes = [*path]
+                    else:
+                        common_nodes = [n for n in common_nodes if n in path]
+            paths_through_common = list(nx.all_simple_paths(self._graph, common_nodes[0], common_nodes[-1]))
+            assert len(paths_through_common) == 1, 'need single pivot path'
+            print('MIMO - multiple input multiple output (single pivot point)')
+            top_node = bottom_node = common_nodes[0]
+            # bottom_node = common_nodes[-1]
+        elif len(inputs) > 1:
+            print('MISO - multiple input single output')
+            top_node = outputs[0]
+        elif len(outputs) > 1:
+            print('SIMO - single input multiple output')
+            bottom_node = inputs[0]
+        else:
+            print('SISO - Single path only')
+            bottom_node = inputs[0]
+
+        offset = 0
+        for root, direction, x_mult in [(top_node, 'down', -1), (bottom_node, 'up', 1)]:
+            if root is None:
+                continue
+            print(f'Drawing {root} {direction}')
+            root.clear()
+            self.graph2tree(root, direction)
+            spacing_x, spacing_y = self.spacing
+            for node in root:
+                y = 0
+                if node.parent:
+                    siblings_and_cousins = node.get_siblings_and_cousins()
+                    if len(siblings_and_cousins) > 1:
+                        idx = siblings_and_cousins.index(node)
+                        dys = [n.size[1] + spacing_y for n in siblings_and_cousins]
+                        span = sum(dys)
+                        cumu_dys = [dys[0]]
+                        for dy in dys[1:]:
+                            cumu_dys.append(cumu_dys[-1] + dy)
+                        for i, dy in enumerate(dys):
+                            cumu_dys[i] -= dy/2
+                        y = cumu_dys[idx] - span/2
+                    else:
+                        y = node.parent.pos[1]
+                node.pos = [(offset + x_mult*node.get_level())*spacing_x, y]
+            offset += 1
 
     def position_edges(self):
-        main_path = nx.dag_longest_path(self._graph)
         for node in self._graph.nodes:
+            y = node.pos[1] - .5*node.size[1]
             in_edges = self._graph.in_edges(node)
-            if not in_edges:
-                continue
-            if len(in_edges) > 1:
-                fract = 1 / len(in_edges)
-                f = 0
-                for edge in sorted(in_edges, key=lambda e: 1.0 if e[0] in main_path and e[1] in main_path else 0.0):
-                    self._graph.edges[edge[0], edge[1]]['r_join_fract'] = f, f+fract
-                    f += fract
+            if in_edges and len(in_edges) > 1:
+                yi = y
+                for l, r in sorted(in_edges, key=lambda e: e[0].pos[1]):
+                    dy = l.size[1]
+                    self._graph.edges[l, r]['r_pos_y'] = yi, yi+dy
+                    yi += dy
+            out_edges = self._graph.out_edges(node)
+            if out_edges and len(out_edges) > 1:
+                yi = y
+                for l, r in sorted(out_edges, key=lambda e: e[1].pos[1]):
+                    dy = r.size[1]
+                    self._graph.edges[l, r]['l_pos_y'] = yi, yi+dy
+                    yi += dy
 
     def draw(self):
         tex = str(self.LATEX_HEAD)
         self.position_nodes()
         self.position_edges()
-        for group in self.groups:
+        for group in sorted(self.groups, key=lambda g: g.zorder):
             tex += '\n' + group.to_tex()
+
         drawn = []
         for edge in self._graph.edges:
             l, r = edge
-            if l not in drawn:
-                tex += '\n' + l.to_tex()
-                # drawn.append(l)
+            # if l not in drawn:
+            tex += '\n' + l.to_tex()
+            # drawn.append(l)
             layers = self._graph.edges[l, r]['layers']
             edge_data = dict(self._graph.edges[l, r])
             tex += '\n' + layers.to_tex(l, r, **edge_data)
-            if r not in drawn and edge_data['r_join_fract'][0] < 0.01:
-                tex += '\n' + r.to_tex()
-                # drawn.append(r)
+            # if r not in drawn and edge_data['r_join_fract'][0] < 0.01:
+            tex += '\n' + r.to_tex()
+            # drawn.append(r)
         tex += '\n' + self.LATEX_TAIL
         compile_tex(tex, self.fn, self.LATEX_COMMAND, self.output_tex_too, convert=self.CONVERT_COMMAND)
