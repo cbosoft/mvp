6f7a6d7786f8ffc3568156b7f0d9f2439d7bff42
Christopher Boyle <chrismjboyle@gmail.com>
2021-11-04 08:20:28 +0000

    Tweak group box margins

--- mvp/group.py
+++ mvp/group.py
@@ -1,16 +1,17 @@
 class Group:
 
-    def __init__(self, name, contents, *, margin=0.5, colour='black!10!white'):
+    def __init__(self, name, contents, *, margin=(0.2, 0.5), colour='black!10!white'):
         self.name = name
         self.contents = contents
         self.margin = margin
         self.colour = colour
 
     def to_tex(self):
-        left = min([c.pos[0] for c in self.contents]) + self.margin
-        right = max([c.pos[0]+c.size[0] for c in self.contents]) - self.margin
-        top = max([c.pos[1]+c.size[1]/2+c.ds for c in self.contents]) + self.margin
-        bottom = min([c.pos[1]-c.size[1]/2-c.ds for c in self.contents]) - self.margin
+        mx, my = self.margin
+        left = min([c.pos[0] for c in self.contents]) + mx
+        right = max([c.pos[0]+c.size[0] for c in self.contents]) - mx
+        top = max([c.pos[1]+c.size[1]/2+c.ds for c in self.contents[1:-1]]) + my
+        bottom = min([c.pos[1]-c.size[1]/2-c.ds for c in self.contents[1:-1]]) - my - (0.1 if self.name else 0)
         rv = f'\\draw[fill={self.colour}] ({left},{top}) rectangle ({right},{bottom});'
         if self.name:
             rv += f'\\node[anchor=south] (foo) at ({left/2+right/2},{bottom}) {{ {self.name} }};'
