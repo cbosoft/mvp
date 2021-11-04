96a45b55e9bdfc3151d8e47783d19c4e54ee8d61
Christopher Boyle <chrismjboyle@gmail.com>
2021-11-04 12:35:21 +0000

    Rotate edge text; allow changing of edge text size

--- mvp/edge.py
+++ mvp/edge.py
@@ -1,13 +1,16 @@
+import math
+
 from .data_node import DataNode
 from .util import pts2tikz
 
 
 class Edge:
 
-    def __init__(self, name='', colour='blue!50!white'):
+    def __init__(self, name='', colour='blue!50!white', text_size='footnotesize'):
         super().__init__()
         self.name = name
         self.colour = colour
+        self.text_size = text_size
 
     def to_tex(self, l: DataNode, r: DataNode, *, r_pos_y=None, l_pos_y=None, **kws) -> str:
         x = (r.pos[0] + l.pos[0])*.5
@@ -21,5 +24,9 @@ class Edge:
             pt4 = pt4[0], l_pos_y[0]
         y = sum([pt1[1], pt2[1], pt3[1], pt4[1]])/4
         tex = '\n' + pts2tikz([pt1, pt2, pt3, pt4, pt1], fill=self.colour, fill_opacity='0.5')
-        tex += f'\\node[anchor=center] (foo) at ({x}, {y}) {{ {self.name} }} ;'
+        dx = pt2[0] - pt1[0]
+        dy = (pt2[1] + pt3[1])*.5 - (pt1[1] + pt4[1])*.5
+        angle = math.atan(dy/dx)*180/math.pi
+        if dx < 0: angle += 180
+        tex += f'\\node[anchor=center, rotate={angle}] (foo) at ({x}, {y}) {{ \\{self.text_size} {self.name} }} ;'
         return tex
