44efca12439f3b4dea0794d42719f839744caf4b
Christopher Boyle <chrismjboyle@gmail.com>
2021-12-01 13:46:01 +0000

    Make groups less obtrusive; let user pick slice for height calc

--- mvp/group.py
+++ mvp/group.py
@@ -1,21 +1,22 @@
 class Group:
 
     def __init__(self, name, contents, *, margin=(0.2, 0.5), colour='black!10!white', zorder=0,
-                 label_pos='lower centre'):
+                 label_pos='lower centre', skip: slice = None):
         self.name = name
         self.contents = contents
         self.margin = margin
         self.colour = colour
         self.zorder = zorder
         self.labelpos = label_pos
+        self.skip = slice(0, -1) if not skip else skip
 
     def to_tex(self):
         mx, my = self.margin
         left = min([c.pos[0] for c in self.contents]) + mx
         right = max([c.pos[0]+c.size[0] for c in self.contents]) - mx
-        top = max([c.pos[1]+c.size[1]/2+c.ds for c in self.contents[1:-1]]) + my
-        bottom = min([c.pos[1]-c.size[1]/2-c.ds for c in self.contents[1:-1]]) - my - (0.1 if self.name else 0)
-        rv = f'\\draw[fill={self.colour}] ({left},{top}) rectangle ({right},{bottom});'
+        top = max([c.pos[1]+c.size[1]/2+c.ds for c in self.contents[self.skip]]) + my
+        bottom = min([c.pos[1]-c.size[1]/2-c.ds for c in self.contents[self.skip]]) - my - (0.1 if self.name else 0)
+        rv = f'\\draw[{self.colour},ultra thick] ({left},{top}) rectangle ({right},{bottom});'
         if self.name:
             ypos, xpos = self.labelpos.split()
             anchor = ('north' if ypos == 'upper' else 'south') + {'centre': '', 'left': ' west', 'right': ' east'}[xpos]
